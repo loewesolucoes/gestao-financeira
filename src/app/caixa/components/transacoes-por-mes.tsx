ecad54d9aec9d6cf3333969dd2f12532f18ecbd3
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-05-24 11:11:44 -0300

    saldos

--- src/app/caixa/components/transacoes-por-mes.tsx
+++ src/app/caixa/components/transacoes-por-mes.tsx
@@ -11,14 +11,17 @@ import { useStorage } from "@/app/contexts/storage";
 
 interface CustomProps {
   periodo: PeriodoTransacoes
+  tableName?: TableNames
   transacoesAcumuladaPorMes?: { [key: string]: TransacoesAcumuladasPorMes; }
 }
 
-export function TransacoesPorMes({ periodo, transacoesAcumuladaPorMes }: CustomProps) {
+export function TransacoesPorMes({ periodo, transacoesAcumuladaPorMes, tableName: tn }: CustomProps) {
   const { isDbOk, repository } = useStorage();
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [transacoes, setTransacoes] = useState<{ [key: string]: Transacoes[] }>({});
+
+  const tableName = tn || TableNames.TRANSACOES
 
   useEffect(() => {
     isDbOk && load();
@@ -31,7 +34,7 @@ export function TransacoesPorMes({ periodo, transacoesAcumuladaPorMes }: CustomP
   }
 
   async function loadTransactions() {
-    const result = await repository.list(TableNames.TRANSACOES, periodo);
+    const result = await repository.list(tableName, periodo);
 
     const dict = result.reduce((previous, next) => {
       const period = moment(next.data).format('YYYY-MM');
@@ -46,37 +49,53 @@ export function TransacoesPorMes({ periodo, transacoesAcumuladaPorMes }: CustomP
     setTransacoes(dict);
   }
 
+  const keysTransacoes = Object.keys(transacoes);
+
   return <section className="periodos">
     {isLoading
       ? <Loader className="align-self-center my-5" />
-      : Object.keys(transacoes).map(key => {
-        const transacoesDoPeriodo = transacoes[key] || [];
-        const acumulado = transacoesAcumuladaPorMes[key] || {} as any;
-        const dataAtual = moment(acumulado.mes, 'YYYY-MM');
-        const dataAtualMenos5Meses = dataAtual.clone().add(-5, 'month');
+      : keysTransacoes.length === 0
+        ? (<div className="alert alert-info my-3" role="alert">Nenhum dado encontrado</div>)
+        : keysTransacoes.map(key => {
+          const transacoesDoPeriodo = transacoes[key] || [];
+          const { acumulado, acumuladoAteOMes } = filterAcumulados(transacoesAcumuladaPorMes, key);
 
-        const acumuladoAteOMes = Object.values(transacoesAcumuladaPorMes || {}).filter(x => {
-          const data = moment(x.mes, 'YYYY-MM');
-
-          return data.isSameOrAfter(dataAtualMenos5Meses) && data.isSameOrBefore(dataAtual);
-        });
-
-        return (
-          <section key={key} className="card my-3">
-            <div className="card-header d-flex justify-content-between align-items-center flex-column flex-lg-row gap-3">
-              <h4 className="m-0">Periodo de: {moment(key, 'YYYY-MM').format('MMMM YYYY')}</h4>
-              <small>Valor em caixa no periodo: {NumberUtil.toCurrency(acumulado.totalAcumulado)}</small>
-              <div className="actions d-flex gap-3">
-                <Link href={`/caixa/editar-mes?month=${key}`} className="btn btn-dark">Editar mês</Link>
-                <Link href={`/caixa/copia?month=${key}`} className="btn btn-dark">Copiar mês</Link>
+          return (
+            <section key={key} className="card my-3">
+              <div className="card-header d-flex justify-content-between align-items-center flex-column flex-lg-row gap-3">
+                <h4 className="m-0">Periodo de: {moment(key, 'YYYY-MM').format('MMMM YYYY')}</h4>
+                <small>Valor em caixa no periodo: {NumberUtil.toCurrency(acumulado.totalAcumulado)}</small>
+                <div className="actions d-flex gap-3">
+                  <Link href={`/caixa/editar-mes?month=${key}`} className="btn btn-dark">Editar mês</Link>
+                  <Link href={`/caixa/copia?month=${key}`} className="btn btn-dark">Copiar mês</Link>
+                </div>
               </div>
-            </div>
-            <div className="card-body d-flex align-items-start flex-column-reverse flex-lg-row justify-content-lg-around">
-              <ListaCaixa transacoesDoPeriodo={transacoesDoPeriodo} />
-              <BalancoDoMes transacoesDoPeriodo={transacoesDoPeriodo} transacoesAcumuladasPorMes={acumuladoAteOMes} />
-            </div>
-          </section>
-        );
-      })}
+              <div className="card-body d-flex align-items-start flex-column-reverse flex-lg-row justify-content-lg-around">
+                <ListaCaixa transacoesDoPeriodo={transacoesDoPeriodo} />
+                {tableName !== TableNames.SALDOS && (
+                  <BalancoDoMes transacoesDoPeriodo={transacoesDoPeriodo} transacoesAcumuladasPorMes={acumuladoAteOMes} />
+                )}
+              </div>
+            </section>
+          );
+        })}
   </section>;
 }
+function filterAcumulados(transacoesAcumuladaPorMes: { [key: string]: TransacoesAcumuladasPorMes; }, key: string) {
+  if (transacoesAcumuladaPorMes == null)
+    return { acumulado: {}, acumuladoAteOMes: [] };
+
+
+  const acumulado = transacoesAcumuladaPorMes[key] || {} as any;
+  const dataAtual = moment(acumulado.mes, 'YYYY-MM');
+  const dataAtualMenos5Meses = dataAtual.clone().add(-5, 'month');
+
+  const acumuladoAteOMes = Object.values(transacoesAcumuladaPorMes || {}).filter(x => {
+    const data = moment(x.mes, 'YYYY-MM');
+
+    return data.isSameOrAfter(dataAtualMenos5Meses) && data.isSameOrBefore(dataAtual);
+  });
+
+  return { acumulado, acumuladoAteOMes };
+}
+
--- src/app/saldos/page.tsx
+++ src/app/saldos/page.tsx
@@ -7,6 +7,7 @@ import { PeriodoForm } from "../caixa/components/periodo-form";
 import { TransacaoForm } from "../caixa/components/transacao-form";
 import { PeriodoTransacoes, TableNames } from "../utils/db-repository";
 import { useEffect, useState } from "react";
+import { TransacoesPorMes } from "../caixa/components/transacoes-por-mes";
 
 function Saldos() {
   const [periodo, setPeriodo] = useState<PeriodoTransacoes>(PeriodoTransacoes.ULTIMO_MES);
@@ -23,6 +24,8 @@ function Saldos() {
           <PeriodoForm onChange={(x: PeriodoTransacoes) => setPeriodo(x)} value={periodo} />
           <TransacaoForm tableName={TableNames.SALDOS} />
         </section>
+
+        <TransacoesPorMes tableName={TableNames.SALDOS} periodo={periodo} />
       </article>
     </main>
   );
