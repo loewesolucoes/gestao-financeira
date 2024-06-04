d953e3e831d135ddec85fcfe302bad4cf302fdaf
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-06-04 10:30:35 -0300

    manifest

--- next.config.js
+++ next.config.js
@@ -49,8 +49,15 @@ const nextConfig = {
 }
 
 const withPWA = require('next-pwa')({
-  dest: 'public/pwa',
+  dest: 'public',
   disable: isDev,
+  fallbacks: {
+    image: `${basePath}/logo.png`,
+    // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  }
 })
 
 module.exports = withPWA(nextConfig);
---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+// src/app/manifest.ts
+
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    "name": process.env.NEXT_PUBLIC_TITLE,
+    "short_name": process.env.NEXT_PUBLIC_TITLE,
+    "start_url": `${process.env.BASE_PATH}/`,
+    "icons": [
+      {
+        "src": `${process.env.BASE_PATH}/android-chrome-192x192.png`,
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": `${process.env.BASE_PATH}/android-chrome-512x512.png`,
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+    "theme_color": "#65a246",
+    "background_color": "#65a246",
+    "display": "standalone"
+  };
+}
