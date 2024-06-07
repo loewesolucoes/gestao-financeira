ed1d672598a390bd44a44d07a100d4e9f8a29497
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-06-07 16:40:03 -0300

    metas

--- src/app/metas/page.tsx
+++ src/app/metas/page.tsx
@@ -15,7 +15,7 @@ import { EnumUtil } from "../utils/enum";
 function MetasPage() {
   const { isDbOk, repository } = useStorage();
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [metas, setMetas] = useState<Metas[]>([]);
+  const [metas, setMetas] = useState<{ [key: string]: Metas[] }>({});
   const [metaAEditar, setMetaAEditar] = useState<Metas>();
 
   useEffect(() => {
@@ -35,10 +35,21 @@ function MetasPage() {
   async function loadMetas() {
     const result = await repository.list<Metas>(TableNames.METAS);
 
-    setMetas(result);
-    console.log(result);
+    const dict = result.reduce((previous, next) => {
+      const period = moment(next.data).format('YYYY-MM');
+      const transOfMonth = previous[period] || [];
 
+      transOfMonth.push(next);
+      previous[period] = transOfMonth;
+
+      return previous
+    }, {} as any);
+
+    setMetas(dict);
   }
+
+
+  const keysMetas = Object.keys(metas);
 
 
   return (
@@ -47,34 +58,43 @@ function MetasPage() {
       <MetasForm />
       {isLoading
         ? <Loader className="align-self-center my-5" />
-        : metas.length === 0
+        : keysMetas.length === 0
           ? (<div className="alert alert-info my-3" role="alert">Nenhum dado encontrado</div>)
-          : (
-            <>
-              <ul className="list-group">
-                {metas.map((x, i) => (
-                  <li key={`${x.data}:${x.descricao}:${i}`} className={`list-group-item ${x.descricao ?? 'list-group-item-info'} ${x.tipo === TipoDeMeta.PESSOAL ? 'list-group-item-success' : ''}  ${x.tipo === TipoDeMeta.FINANCEIRA ? 'list-group-item-warning' : ''}`}>
-                    <div className="d-flex w-100 justify-content-between gap-3">
-                      <div className="d-flex flex-column gap-3">
-                        <h5>{x.descricao}</h5>
-                        <p>{x.comentario}</p>
-                      </div>
-                      <div className="d-flex flex-column gap-3">
-                        <small>{moment(x.data).format('MMMM YYYY')}</small>
-                        <button className="btn btn-secondary" onClick={e => setMetaAEditar(x)}>Editar</button>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-              {metaAEditar && (
-                <Modal hideFooter={true} onClose={() => setMetaAEditar(null)} title={`Detalhes da transação: ${metaAEditar?.descricao}`}>
-                  <MetasForm meta={metaAEditar} cleanStyle={true} onClose={() => setMetaAEditar(null)} />
-                </Modal>
-              )}
-            </>
-          )
-      }
+          : keysMetas.map(key => {
+            const metasDoPeriodo = metas[key] || [];
+            const momentPeriod = moment(key, 'YYYY-MM');
+
+            return (
+              <section key={key} className="card my-3">
+                <div className="card-header d-flex justify-content-between align-items-center flex-column flex-lg-row gap-3">
+                  <h4 className="m-0">Periodo de: {momentPeriod.format('YYYY')}</h4>
+                </div>
+                <div className="card-body">
+                  <ul className="list-group">
+                    {metasDoPeriodo.map((x, i) => (
+                      <li key={`${x.data}:${x.descricao}:${i}`} className={`list-group-item ${x.descricao ?? 'list-group-item-info'} ${x.tipo === TipoDeMeta.PESSOAL ? 'list-group-item-success' : ''}  ${x.tipo === TipoDeMeta.FINANCEIRA ? 'list-group-item-warning' : ''}`}>
+                        <div className="d-flex w-100 justify-content-between gap-3">
+                          <div className="d-flex flex-column gap-3">
+                            <h5>{x.descricao}</h5>
+                            <p>{x.comentario}</p>
+                          </div>
+                          <div className="d-flex flex-column gap-3">
+                            <small>{moment(x.data).format('MMMM YYYY')}</small>
+                            <button className="btn btn-secondary" onClick={e => setMetaAEditar(x)}>Editar</button>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                  {metaAEditar && (
+                    <Modal hideFooter={true} onClose={() => setMetaAEditar(null)} title={`Detalhes da transação: ${metaAEditar?.descricao}`}>
+                      <MetasForm meta={metaAEditar} cleanStyle={true} onClose={() => setMetaAEditar(null)} />
+                    </Modal>
+                  )}
+                </div>
+              </section>
+            )
+          })}
     </main>
   );
 }
