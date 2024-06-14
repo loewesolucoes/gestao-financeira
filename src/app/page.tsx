9bef96cf1a09aadd0e02c7038832210ea65503d1
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-06-14 09:33:20 -0300

    add grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,92 +61,88 @@ function Home() {
               ? (<div className="alert alert-info" role="alert">Nenhum dado encontrado</div>)
               : (
                 <>
-                  <section className="d-flex flex-column gap-3">
-                    <section className="card border-primary">
-                      <h4 className="card-header">Caixa Geral</h4>
-                      <div className="card-body">
-                        <div className="d-flex gap-3">
-                          <h5>Valor em caixa</h5>
-                          <div className="d-flex flex-column">
-                            <p className="m-0">{NumberUtil.toCurrency(valorEmCaixa)}</p>
-                            <small>{NumberUtil.extenso(valorEmCaixa)}</small>
-                          </div>
-                        </div>
-                        <div className="d-flex gap-3 mt-3">
-                          <h5>Escolha um mês</h5>
-                          <div className="form-floating">
-                            <Input type="month" className="form-control" id="data" placeholder="Mês a aplicar" value={yearAndMonth} onChange={x => setYearAndMonth(x)} />
-                            <label htmlFor="data" className="form-label">Mês a aplicar</label>
-                          </div>
+                  <section className="card border-primary card-caixa">
+                    <h4 className="card-header">Caixa Geral</h4>
+                    <div className="card-body">
+                      <div className="d-flex gap-3">
+                        <h5>Valor em caixa</h5>
+                        <div className="d-flex flex-column">
+                          <p className="m-0">{NumberUtil.toCurrency(valorEmCaixa)}</p>
+                          <small>{NumberUtil.extenso(valorEmCaixa)}</small>
                         </div>
                       </div>
-                    </section>
-                    <section className="card border-dark">
-                      <h4 className="card-header">Caixa do mês: {moment(yearAndMonth).format('MMMM YYYY')}</h4>
-                      <div className="card-body">
-                        {sobra == null
-                          ? (<div className="alert alert-info" role="alert">Mês sem dados</div>)
-                          : (
-                            <>
-                              <div className="d-flex gap-3">
-                                <h5>Receitas:</h5>
-                                <div className="d-flex flex-column">
-                                  <p className="m-0">{NumberUtil.toCurrency(receitas)}</p>
-                                  <small>{NumberUtil.extenso(receitas)}</small>
-                                </div>
-                              </div>
-                              <div className="d-flex gap-3">
-                                <h5>Depesas:</h5>
-                                <div className="d-flex flex-column">
-                                  <p className="m-0">{NumberUtil.toCurrency(despesas)}</p>
-                                  <small>{NumberUtil.extenso(despesas)}</small>
-                                </div>
-                              </div>
-                              <div className="d-flex gap-3">
-                                <h5>Sobra:</h5>
-                                <div className="d-flex flex-column">
-                                  <p className="m-0">{NumberUtil.toCurrency(sobra)}</p>
-                                  <small>{NumberUtil.extenso(sobra)}</small>
-                                </div>
-                              </div>
-                            </>
-                          )}
+                      <div className="d-flex gap-3 mt-3 flex-column flex-xlg-row">
+                        <h5>Escolha um mês</h5>
+                        <div className="form-floating">
+                          <Input type="month" className="form-control" id="data" placeholder="Mês a aplicar" value={yearAndMonth} onChange={x => setYearAndMonth(x)} />
+                          <label htmlFor="data" className="form-label">Mês a aplicar</label>
+                        </div>
                       </div>
-                    </section>
-                    <section className="card border-primary">
-                      <h4 className="card-header">Metas</h4>
-                      <div className="card-body">
-                        {(metas == null || metas.length === 0) && (<div className="alert alert-info" role="alert">Nenhuma meta cadastrada</div>)}
-                        <ul className="list-group">
-                          {metas?.map((x, i) => (
-                            <li key={`${x.data}:${x.descricao}:${i}`} className={`list-group-item ${x.descricao ?? 'list-group-item-info'} ${x.tipo === TipoDeMeta.PESSOAL ? 'list-group-item-success' : ''}  ${x.tipo === TipoDeMeta.FINANCEIRA ? 'list-group-item-warning' : ''}`}>
-                              <div className="d-flex w-100 justify-content-between gap-3">
-                                <div className="d-flex flex-column gap-3">
-                                  <h6>{x.descricao}</h6>
-                                </div>
-                                <div className="d-flex flex-column gap-3">
-                                  <small>{moment(x.data).format('MMMM YYYY')}</small>
-                                </div>
-                              </div>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </section>
+                    </div>
                   </section>
-                  <section className="d-flex flex-column gap-3">
-                    <section className="card border-info">
-                      <h4 className="card-header">Caixa acumulado mês a mês</h4>
-                      <div className="card-body">
-                        <GraficoCaixaAcumuladoMesAMes transacoesAcumuladasPorMes={transacoesAcumuladaPorMes} />
-                      </div>
-                    </section>
-                    <section className="card border-info">
-                      <h4 className="card-header">Balanço mês a mês</h4>
-                      <div className="card-body">
-                        <GraficoBalancoMesAMes transacoesAcumuladasPorMes={transacoesAcumuladaPorMes} />
-                      </div>
-                    </section>
+                  <section className="card border-dark">
+                    <h4 className="card-header">Caixa do mês: {moment(yearAndMonth).format('MMMM YYYY')}</h4>
+                    <div className="card-body">
+                      {sobra == null
+                        ? (<div className="alert alert-info" role="alert">Mês sem dados</div>)
+                        : (
+                          <>
+                            <div className="d-flex gap-3">
+                              <h5>Receitas:</h5>
+                              <div className="d-flex flex-column">
+                                <p className="m-0">{NumberUtil.toCurrency(receitas)}</p>
+                                <small>{NumberUtil.extenso(receitas)}</small>
+                              </div>
+                            </div>
+                            <div className="d-flex gap-3">
+                              <h5>Depesas:</h5>
+                              <div className="d-flex flex-column">
+                                <p className="m-0">{NumberUtil.toCurrency(despesas)}</p>
+                                <small>{NumberUtil.extenso(despesas)}</small>
+                              </div>
+                            </div>
+                            <div className="d-flex gap-3">
+                              <h5>Sobra:</h5>
+                              <div className="d-flex flex-column">
+                                <p className="m-0">{NumberUtil.toCurrency(sobra)}</p>
+                                <small>{NumberUtil.extenso(sobra)}</small>
+                              </div>
+                            </div>
+                          </>
+                        )}
+                    </div>
+                  </section>
+                  <section className="card border-primary card-metas">
+                    <h4 className="card-header">Metas</h4>
+                    <div className="card-body">
+                      {(metas == null || metas.length === 0) && (<div className="alert alert-info" role="alert">Nenhuma meta cadastrada</div>)}
+                      <ul className="list-group">
+                        {metas?.map((x, i) => (
+                          <li key={`${x.data}:${x.descricao}:${i}`} className={`list-group-item ${x.descricao ?? 'list-group-item-info'} ${x.tipo === TipoDeMeta.PESSOAL ? 'list-group-item-success' : ''}  ${x.tipo === TipoDeMeta.FINANCEIRA ? 'list-group-item-warning' : ''}`}>
+                            <div className="d-flex w-100 justify-content-between gap-3">
+                              <div className="d-flex flex-column gap-3">
+                                <h6>{x.descricao}</h6>
+                              </div>
+                              <div className="d-flex flex-column gap-3">
+                                <small>{moment(x.data).format('MMMM YYYY')}</small>
+                              </div>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </section>
+                  <section className="card border-info card-chart">
+                    <h4 className="card-header">Caixa acumulado mês a mês</h4>
+                    <div className="card-body">
+                      <GraficoCaixaAcumuladoMesAMes transacoesAcumuladasPorMes={transacoesAcumuladaPorMes} />
+                    </div>
+                  </section>
+                  <section className="card border-info card-chart">
+                    <h4 className="card-header">Balanço mês a mês</h4>
+                    <div className="card-body">
+                      <GraficoBalancoMesAMes transacoesAcumuladasPorMes={transacoesAcumuladaPorMes} />
+                    </div>
                   </section>
                 </>
               )
