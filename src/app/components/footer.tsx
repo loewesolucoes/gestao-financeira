2074099bad7e57215c3d82215700e79dc60d59dd
Érico de Souza Loewe <ericoloewe@gmail.com>
2025-06-16 23:02:49 -0300

    versao

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useEnv } from "../contexts/env";
 
 export function Footer() {
@@ -11,7 +12,8 @@ export function Footer() {
 }
 
 function Copyright({ sm }: any) {
-  const { packageVersion } = useEnv();
-  return <p className={`copyright ${sm ? 'sm' : 'lg'}`}>&copy; {new Date().getFullYear()}&nbsp;-&nbsp;<a href="https://loewesolucoes.github.io/">@ericoloewe</a> Versão: {packageVersion}</p>;
+  const { version } = useEnv();
+
+  return <p className={`copyright ${sm ? 'sm' : 'lg'}`}>&copy; {new Date().getFullYear()}&nbsp;-&nbsp;<a href="https://loewesolucoes.github.io/">@ericoloewe</a> Versão atual: <Link href={`https://github.com/loewesolucoes/gestao-financeira/releases/tag/v${version}`} target="_blank">v{version}</Link></p>;
 }
 
